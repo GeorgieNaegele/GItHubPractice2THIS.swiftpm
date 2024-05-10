a4acb48720222a658870d325508264f3b17cb360
georgie naegele <gnaegele5254@stu.d214.org>
2024-05-10 15:17:34 -0500

    no noun view was made (because I was working alone) so I did the next step in the verb view, where I added a binding object of type madlib and a textfeild in the body

--- VerbView.swift
+++ VerbView.swift
@@ -7,9 +7,13 @@
 
 import SwiftUI
 
+@Binding var Madlib: MadLib()
+
 struct VerbView: View {
     var body: some View {
         VStack {
+            TextField("Enter Verb HERE", text: $Madlib)
+            
             NavigationView {
                 NavigationLink("Adjective View", destination: AdjectiveView())
             }
