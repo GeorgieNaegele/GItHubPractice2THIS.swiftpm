1f2fa3566886631fa486614a81d1932d93f072ac
georgie naegele <gnaegele5254@stu.d214.org>
2024-05-10 15:14:07 -0500

    made a summary view and a dummy sentence

---
+++ SummaryView.swift
@@ -0,0 +1,17 @@
+//
+//  SummaryView.swift
+//  GItHubPractice2THIS
+//
+//  Created by Georgie Naegele on 5/10/24.
+//
+
+import SwiftUI
+
+struct SummaryView: View {
+    var body: some View {
+        VStack {
+           Text( "The " + "noun"+ " wanted to "+ "verb" + " but the "+"adjective" + " dragon " + " wouldn't allow it")
+            }
+        }
+    }
+
