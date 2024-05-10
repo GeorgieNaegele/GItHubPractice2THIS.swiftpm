ee8c70a44aa50d8acbb45e2fd65b2a9f6f05da3c
georgie naegele <gnaegele5254@stu.d214.org>
2024-05-10 15:19:19 -0500

    did the same as the last step but for the "adj view"

--- SummaryView.swift
+++ SummaryView.swift
@@ -7,9 +7,14 @@
 
 import SwiftUI
 
+@Binding var Madlib: MadLib()
+
 struct SummaryView: View {
     var body: some View {
         VStack {
+            
+            TextField("Enter adjective HERE", text: $Madlib)
+            
            Text( "The " + "noun"+ " wanted to "+ "verb" + " but the "+"adjective" + " dragon " + " wouldn't allow it")
             }
         }
